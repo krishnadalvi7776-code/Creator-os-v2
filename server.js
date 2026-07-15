ab0f0bee17f2d8c0fe4487a662d84a17e96079a8
krishna Dalvi <krishnadalvi7776@gmail.com>
2026-07-15 15:35:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post("/chat", async (req, res) => {
     const { message } = req.body;
 
     const response = await ai.models.generateContent({
-      model: "gemini-2.5-flash",
+      model: "gemini-2.5-flash-lite",
       contents: message,
     });
 
