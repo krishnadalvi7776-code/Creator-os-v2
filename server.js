ffb2501f878dd0953adecce4a5c260a2959741d7
krishna Dalvi <krishnadalvi7776@gmail.com>
2026-07-17 00:46:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { GoogleGenAI } from "@google/genai";
-
+import fetch from "node-fetch";
 dotenv.config();
 
 const app = express();
