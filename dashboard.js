f1c185724cc4b6186c4325e97ab8dbe108579874
krishna Dalvi <krishnadalvi7776@gmail.com>
2026-07-16 09:30:58 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,6 @@
 document.getElementById("openChat").addEventListener("click", () => {
   window.location.href = "ai-chat.html";
 }); 
+document.getElementById("openImage").addEventListener("click", () => {
+  window.location.href = "ai-image.html";
+});
