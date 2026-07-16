cec6797febdf258b4d6ad4ea5b2ef868979ba47a
krishna Dalvi <krishnadalvi7776@gmail.com>
2026-07-16 13:05:10 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -4,3 +4,28 @@ document.getElementById("openChat").addEventListener("click", () => {
 document.getElementById("openImage").addEventListener("click", () => {
   window.location.href = "ai-image.html";
 });
+const openChat = document.getElementById("openChat");
+
+if(openChat){
+  openChat.onclick = () => {
+    window.location.href = "ai-chat.html";
+  };
+}
+
+
+const openImage = document.getElementById("openImage");
+
+if(openImage){
+  openImage.onclick = () => {
+    window.location.href = "ai-image.html";
+  };
+}
+
+
+const openVideo = document.getElementById("openVideo");
+
+if(openVideo){
+  openVideo.onclick = () => {
+    window.location.href = "ai-video.html";
+  };
+}
